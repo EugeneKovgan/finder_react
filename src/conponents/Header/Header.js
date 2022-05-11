30bbc3195b11701ab6af7d5e518672974dc0dd14
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-05-11 23:42:22 +0300

    feat: started task and have started head component

---
+++ src/conponents/Header/Header.js
@@ -0,0 +1,19 @@
+import './Header.scss';
+import logo_git from '../../assets/svg/git_icon.svg';
+import search_icon from '../../assets/svg/search_icon.svg';
+const Header = () => {
+    return (
+        <div className='Header'>
+            <div className='header_content-container'>
+                <div className='logo-img'>
+                    <img src={logo_git} alt='' />
+                </div>
+                <div className='header-search'>
+                    <input className='header-search_input' type='text' name='' id='' placeholder='gaearon|' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
